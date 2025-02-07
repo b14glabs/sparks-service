7e8e6a4c43e4fde78cfd47ac88805b87de168415
nguyenHoang32 <nguyenhuuhoang2k15@gmail.com>
2025-02-07 16:35:26 +0700

    remove key sparks col

--- src/models/sparks-point.model.ts
+++ src/models/sparks-point.model.ts
@@ -31,32 +31,6 @@ const pointSchema = new Schema(schemaDefinition, {
   collection: 'sparks_point',
 })
 pointSchema.plugin(paginate)
-// for dual core
-pointSchema.index(
-  {
-    time: 1,
-    holder: 1,
-    type: 1,
-  },
-  {
-    unique: true,
-    partialFilterExpression: {
-      time: { $type: 'date' },
-      type: { $eq: 'dual-core-snapshot' },
-    },
-  }
-)
-//pointSchema.index({
-//  type: 1,
-//  holder: 1,
-//  createdAt: 1,
-//  point: 1,
-//  receiver: 1,
-//  txId: 1,
-//}, {
-//  unique: true,
-//  sparse: true
-//})
 
 export type ISparksPoint = InferRawDocType<typeof schemaDefinition>
 export const SparksPoint = model('Sparks-Point', pointSchema)
